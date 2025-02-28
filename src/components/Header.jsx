c5d484c85c5e5ad7441b441c83a9c2063863fe44
frankamg487 <fkamgang06@gmail.com>
2025-02-28 12:04:11 +0200

    navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="/download.png" alt="" />
+      <div className="logement">
+        <li>
+          <a href="">Logement</a>
+        </li>
+        <li>
+          <a href="">Experiences</a>
+        </li>
+      </div>
+      <div className="connexion">
+        <li>
+          <a href="">Mettre mon logement sur Aibnb</a>
+        </li>
+        <li>
+          <a href="">
+            {" "}
+            <i className="fa-solid fa-globe"></i>
+          </a>
+        </li>
+        <div className="burger">
+          <a href="">
+             <i className="fa-solid fa-bars"></i>
+         </a>
+          <a href="">
+             <i className="fa-solid fa-user"></i>
+         </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
